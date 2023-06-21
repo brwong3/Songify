000039971475dae483675d66f365372ddbf4d57e
Brandon <Brandon@Brandons-Laptop.hsd1.ca.comcast.net>
2023-06-21 00:41:03 -0700

    Initial Commit + Started on Login Screen

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../styles/SignIn.css"
+import { motion } from "framer-motion"
+
+
+export default function SignIn() {
+  return (
+    <div className='SignIn'>
+      <h1>Songify</h1>
+      <h4>For All Of Your Listening Needs</h4>
+      <motion.div 
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.3,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01]
+        }} 
+        className='SignIn-Container'>
+        
+        <p>Email:</p>
+        <input></input>
+
+      </motion.div>
+
+    </div>
+  )
+}
