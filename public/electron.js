a21e72166681c005417f1658eef4f7a0c6ecd6bd
Brandon <Brandon@Brandons-Laptop.hsd1.ca.comcast.net>
2023-09-05 23:46:25 -0700

    Complete With Build

---
+++ public/electron.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+require("../Backend/server")
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 750,
+    minWidth: 1000,
+    minHeight: 750,
+    webPreferences: {
+        nodeIntegration: true
+    }
+ });
+mainWindow.loadURL(
+  isDev
+  ? "http://localhost:3000"
+  : `file://${path.join(__dirname, "../build/index.html")}`
+ );
+if (isDev) {
+  mainWindow.webContents.openDevTools();
+ }
+}
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
